072e3fe993da5cfccd0de6cd1eaa00959b1d1257
thuanpv1 <thuanpv@egt.vn>
2021-11-25 17:08:09 +0700

    commit

--- CertificateCreator/Program.cs
+++ CertificateCreator/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Security.Cryptography.X509Certificates;
 
 namespace CertificateCreator
 {
@@ -22,6 +23,25 @@ namespace CertificateCreator
             {
                 binWriter.Write(certificateData);
                 binWriter.Flush();
+
+                // Create a collection object and populate it using the PFX file
+                X509Certificate2Collection collection = new X509Certificate2Collection();
+                collection.Import(certificateData, password, X509KeyStorageFlags.PersistKeySet);
+
+                foreach (X509Certificate2 cert in collection)
+                {
+                    Console.WriteLine("Subject is: '{0}'", cert.Subject);
+                    Console.WriteLine("Issuer is:  '{0}'", cert.Issuer);
+
+                    // Import the certificate into an X509Store object
+                    var store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+                    store.Open(OpenFlags.ReadWrite);
+                    if (!store.Certificates.Contains(cert))
+                    {
+                        store.Add(cert);
+                    }
+                    store.Close();
+                }
             }
         }
 
